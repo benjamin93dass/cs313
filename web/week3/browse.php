0a64df5985730ab78fbc2192158b9e5b7a9bc848
Benjamin Dass <bdas16001@gmail.com>
2019-09-29 19:44:33 -0600

    initialized browse and added path from assignment

---
+++ web/week3/browse.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Browse</title>
+        <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700%7CRoboto%7CJosefin+Sans:100,300,400,500" rel="stylesheet" type="text/css">
+    </head>
+    <body>
+        <p>Welcome. Please browse.</p>
+    </body>
+</html>
