bd6b8ddc206a1380c5300060cdf0c5685b5777f0
Benjamin Dass <bdas16001@gmail.com>
2019-10-17 14:44:55 -0600

    bugs 3

--- web/week5/index.php
+++ web/week5/index.php
@@ -40,7 +40,7 @@
   <div class="d-flex" id="wrapper">
 
     <!-- Sidebar -->
-    <div class="bg-light border-right" id="sidebar-wrapper">
+    <div class="bg-light border-right" id="sidebar-wrapper" class="toggled">
       <div class="sidebar-heading">Menu</div>
       <div class="list-group list-group-flush">
         <a href="index.php" class="list-group-item list-group-item-action bg-light">Summary of Accounts</a>
