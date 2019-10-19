14e965aded597bce13703344e9aa887b768fd304
Benjamin Dass <bdas16001@gmail.com>
2019-10-19 16:52:45 -0600

    adding 3rd table info 2

--- web/week5/index.php
+++ web/week5/index.php
@@ -77,7 +77,7 @@
     <div id="sidebar-wrapper">
       <ul class="sidebar-nav">
                 <li class="sidebar-brand">
-                    <span style='display:inline'><i>Current user: </i><b><a href="#"><?php echo"  $current_username "?></a></b></span>
+                <?php echo "<span style='display:inline'><i>Current user: </i><b> $current_username </b></span>" ?>
                 </li>
                 <li>
                   <a href="index.php">Summary of Accounts</a>
