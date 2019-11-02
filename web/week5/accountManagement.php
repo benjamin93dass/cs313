83ab46b5f6e1637151f4ede92101fc5bbc0ca5b8
Benjamin Dass <bdas16001@gmail.com>
2019-11-02 14:41:42 -0600

    minor bugs 32

--- web/week5/accountManagement.php
+++ web/week5/accountManagement.php
@@ -92,7 +92,7 @@
           <?php
               foreach ($bank_names as $bank_name) {
                 $name = $bank_name['bank_name'];
-                echo "<input type='radio'>" . $name;
+                echo "<label class='btn btn-secondary' style='margin-right:15px;'><input type='radio' name='nName' value='' autocomplete='off'>" . $name . "</label>";
               }
           ?>
           <input type="submit" value="Update entries">
