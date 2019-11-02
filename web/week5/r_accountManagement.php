d2d5df125dd1631797941e7e96bc68a9f5d37345
Benjamin Dass <bdas16001@gmail.com>
2019-11-02 16:50:29 -0600

    minor bugs 2

--- web/week5/r_accountManagement.php
+++ web/week5/r_accountManagement.php
@@ -35,26 +35,26 @@
     try {
         // check for change in debit balance
         if ($deb_bal != 0) {
-            $update_query = 'UPDATE bank_account SET debit_balance=:deb_bal WHERE bank_account=:bank_account AND name=1';
+            $update_query = 'UPDATE bank_account SET debit_balance=:deb_bal WHERE bank_name=:bank_name AND name=1';
             $stmt = $db->prepare($update_query);
             $stmt->bindValue(':deb_bal', $deb_bal);
-            $stmt->bindValue(':bank_account', $bank_account);
+            $stmt->bindValue(':bank_account', $bank_name);
             $stmt->execute();
         }
         // check for change in available credit
         if ($aval_cre != 0) {
-            $update_query = 'UPDATE bank_account SET available_credit=:aval_cre WHERE bank_account=:bank_account AND name=1';
+            $update_query = 'UPDATE bank_account SET available_credit=:aval_cre WHERE bank_name=:bank_name AND name=1';
             $stmt = $db->prepare($update_query);
             $stmt->bindValue(':aval_cre', $aval_cre);
-            $stmt->bindValue(':bank_account', $bank_account);
+            $stmt->bindValue(':bank_account', $bank_name);
             $stmt->execute();
         }
         // check for change in credit balance
         if ($cre_bal != 0) {
-            $update_query = 'UPDATE bank_account SET credit_balance=:cre_bal WHERE bank_account=:bank_account AND name=1';
+            $update_query = 'UPDATE bank_account SET credit_balance=:cre_bal WHERE bank_name=:bank_name AND name=1';
             $stmt = $db->prepare($update_query);
             $stmt->bindValue(':cre_bal', $cre_bal);
-            $stmt->bindValue(':bank_account', $bank_account);
+            $stmt->bindValue(':bank_account', $bank_name);
             $stmt->execute();
         }
     } catch (Exception $ex) {
