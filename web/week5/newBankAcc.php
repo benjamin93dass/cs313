bd5f7372a208937bf1735e7eab30e89cd5ed7610
Benjamin Dass <bdas16001@gmail.com>
2019-11-02 12:30:13 -0600

    minor bugs 3

--- web/week5/newBankAcc.php
+++ web/week5/newBankAcc.php
@@ -77,32 +77,32 @@
           <p>Current banks we serve:
             <div class="btn-group btn-group-toggle" data-toggle="buttons">
               <label class="btn btn-secondary active">
-                <input type="radio" autocomplete="off"> Wells Fargo |
-              </label>
+                <input type="radio" autocomplete="off"> Wells Fargo
+              </label> |
               <label class="btn btn-secondary">
-                <input type="radio" autocomplete="off"> Citi |
-              </label>
+                <input type="radio" autocomplete="off"> Citi
+              </label> |
               <label class="btn btn-secondary">
-                <input type="radio" autocomplete="off"> US Bankcorp/U.S. Bank |
-              </label>
+                <input type="radio" autocomplete="off"> US Bankcorp/U.S. Bank
+              </label> |
               <label class="btn btn-secondary">
-                <input type="radio" autocomplete="off"> PNC |
-              </label>
+                <input type="radio" autocomplete="off"> PNC
+              </label> |
               <label class="btn btn-secondary">
-                <input type="radio" autocomplete="off"> Bank of New York Mellon |
-              </label>
+                <input type="radio" autocomplete="off"> Bank of New York Mellon
+              </label> |
               <label class="btn btn-secondary">
-                <input type="radio" autocomplete="off"> State Street |
-              </label>
+                <input type="radio" autocomplete="off"> State Street
+              </label> |
               <label class="btn btn-secondary">
-                <input type="radio" autocomplete="off"> Capital One |
-              </label>
+                <input type="radio" autocomplete="off"> Capital One
+              </label> |
               <label class="btn btn-secondary">
-                <input type="radio" autocomplete="off"> TD Bank |
-              </label>
+                <input type="radio" autocomplete="off"> TD Bank
+              </label> |
               <label class="btn btn-secondary">
-                <input type="radio" autocomplete="off"> Mountain America |
-              </label>
+                <input type="radio" autocomplete="off"> Mountain America
+              </label> |
               <label class="btn btn-secondary">
                 <input type="radio" autocomplete="off"> BeeHive FCU
               </label>
